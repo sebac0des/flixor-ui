881188150a8590324c1a8a21dca98ded0c91f495
rocketup.designs <rocketup.designs@gmail.com>
2024-06-14 21:11:11 +0000

    component command function added

--- src/commands/component.ts
+++ src/commands/component.ts
@@ -1,13 +1,33 @@
 import {Command} from 'commander'
-
+import prompts from 'prompts'
+import chalk from 'chalk'
 
 export const component = new Command('component')
   .description('add component to your project')
   .action(() => {
-    console.log('This is a fake command.')
+    try {
+      PromptForConfig()
+    }catch(e){}
   })
 
+export async function PromptForConfig(){
 
+  const highlight = (text:string)=>chalk.green(text)
+
+  const options = await prompts([
+    {
+      type: "select",
+      name: "component",
+      message: `Which ${highlight("component")} would you like to install?`,
+      choices: [
+        { title: "Button", value: "button" },
+        { title: "Input", value: "input" },
+      ],
+    },
+  ])
+
+  
+}
 
 
 

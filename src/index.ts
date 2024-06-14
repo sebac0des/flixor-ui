1d97de5efa1311cb36d6b9f98fd552638524cac0
rocketup.designs <rocketup.designs@gmail.com>
2024-06-14 18:02:29 +0000

    base config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
+import {init} from '@/src/commands/init'
+import {Command} from 'commander'
 
 async function main(){
-    console.log('Hello World')
+    const program = new Command()
+    .name("flixor ui")
+    .description("Elevate content creation with my CLI's specialized web templates.")
+    .version(
+      "1.0.0",
+      "-v, --version",
+      "display the version number"
+    )
+
+    program.addCommand(init)
+
+    program.parse()
 
     return null
 }
